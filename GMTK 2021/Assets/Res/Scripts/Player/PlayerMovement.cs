644289995f0b4413edcab29605e60b3700d927cd
jack leslie <60219781+JackLeslie432@users.noreply.github.com>
2021-06-11 19:49:01 +0100

    Imput and Player
    -Master Input Added
    -Player scene and script added

---
+++ GMTK 2021/Assets/Res/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private int speed;
+
+    private Vector2 velocity;
+
+    private bool direction;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+    }
+}
