51a89334eeb7851e25c9f9e9a45633da9823ef5a
Matthew Rennie <36052246+Matthew-Rennie@users.noreply.github.com>
2021-06-11 23:12:18 +0100

    doors

--- GMTK 2021/Assets/Res/Scripts/Object Nodes/Door.cs
+++ GMTK 2021/Assets/Res/Scripts/Object Nodes/Door.cs
@@ -2,6 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : MonoBehaviour
+namespace Nodes
 {
+    public class Door : Node
+    {
+        private BoxCollider2D boxcollider;
+
+        private void Awake()
+        {
+            boxcollider = GetComponent<BoxCollider2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (IsPowered())
+            {
+                boxcollider.enabled = false;
+            }
+            else
+            {
+                boxcollider.enabled = true;
+            }
+        }
+    }
 }
