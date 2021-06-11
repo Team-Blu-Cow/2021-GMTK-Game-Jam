cb980204eea18b5e35453a94312e2a402fe09e1d
Matthew Rennie <36052246+Matthew-Rennie@users.noreply.github.com>
2021-06-11 21:25:11 +0100

    2 connections can be connected to each other

--- GMTK 2021/Assets/Res/Scripts/Object Nodes/Connections/InputConnection.cs
+++ GMTK 2021/Assets/Res/Scripts/Object Nodes/Connections/InputConnection.cs
@@ -7,5 +7,56 @@ namespace Nodes
     [System.Serializable]
     public class InputConnection : NodeConnection
     {
+        public bool Connect(OutputConnection conn)
+        {
+            if (conn == null)
+                return false;
+
+            GameObject connParent = conn.transform.parent.gameObject;
+            GameObject thisParent = transform.parent.gameObject;
+            
+
+            if (connParent == null)
+                return false;
+
+            if (connParent == thisParent)
+                return false;
+
+            if (conn.HasConnection)
+                return false;
+
+            if (CheckForCircularConnection(conn))
+                return false;
+
+            conn.SetConnection(this);
+            SetConnection(conn);
+
+
+            return true;
+        }
+
+        // return true if there is an error
+        private bool CheckForCircularConnection(OutputConnection conn)
+        {
+            Queue<GameObject> toVisit = new Queue<GameObject>();
+
+            toVisit.Enqueue(conn.transform.parent.gameObject);
+            while (toVisit.Count > 0)
+            {
+                GameObject current = toVisit.Dequeue();
+
+                if (current == this)
+                    return true;
+
+                InputConnection[] conns = current.GetComponents<InputConnection>();
+                for (int i = 0; i < conns.Length; i++)
+                {
+                    toVisit.Enqueue(conns[i].gameObject);
+                }
+            }
+
+            return false;
+        }
+
     }
 }
