a20398036d92a04340a56ff8e73517a6ae638195
jack leslie <60219781+JackLeslie432@users.noreply.github.com>
2021-06-13 01:15:01 +0100

    Added Text to menu

--- GMTK 2021/Assets/HideButtons.cs
+++ GMTK 2021/Assets/HideButtons.cs
@@ -1,15 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HideButtons : MonoBehaviour
 {
+    private CanvasTool.CanvasManager canvasManager;
+
+    private void Start()
+    {
+        canvasManager = FindObjectOfType<CanvasTool.CanvasManager>();
+        OpenAll();
+    }
+
     public void CloseAll()
     {
         for (int i = 0; i < transform.childCount; i++)
         {
             transform.GetChild(i).gameObject.SetActive(false);
         }
+
+        //canvasManager.CloseCanvas();
     }
 
     public void OpenAll()
@@ -18,5 +26,7 @@ public class HideButtons : MonoBehaviour
         {
             transform.GetChild(i).gameObject.SetActive(true);
         }
+
+        canvasManager.OpenCanvas(canvasManager.GetCanvasContainer("Buttons"));
     }
 }
