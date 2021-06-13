315d278e7a0e57a7ffc6b9fbf88eec666d7f7003
Adam Emslie <44575716+yaboiadam00@users.noreply.github.com>
2021-06-13 12:49:00 +0100

    added msuci to the template scene

---
+++ GMTK 2021/Assets/Res/Scripts/Test/TunesMachine.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TunesMachine : MonoBehaviour
+{
+    private float ratio = 0f;
+
+    // Start is called before the first frame update
+    private bool track = false;
+
+    private bool done = true;
+
+    private void Start()
+    {
+        bluModule.Application.instance.audioModule.PlayMusicEvent("event:/music/Main theme");
+    }
+
+    public void SwapTrack()
+    {
+        if (done)
+        {
+            done = false;
+            StartCoroutine(lerpTunes(track));
+        }
+    }
+
+    // Update is called once per frame
+    private IEnumerator lerpTunes(bool in_track)
+    {
+        float currentTime = 0;
+        if (in_track) // lerp to 1
+        {
+            while (currentTime < 1f)
+            {
+                currentTime += Time.deltaTime;
+                ratio = Mathf.Lerp(0, 1, currentTime / 1f);
+                yield return null;
+            }
+        }
+        else // lerp to 0
+        {
+            while (currentTime < 1f)
+            {
+                currentTime += Time.deltaTime;
+                ratio = Mathf.Lerp(1, 0, currentTime / 1f);
+                yield return null;
+            }
+        }
+
+        done = true;
+        yield break;
+    }
+}
